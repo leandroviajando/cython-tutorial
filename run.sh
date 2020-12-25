ea9880b9583cd6540440962bd077016513fc751c
Leander Rysanek <leander.rysanek@derivco.se>
2020-12-25 13:49:04 +0100

    Add black, flake8, isort and pretty_errors

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-python3 setup.py -q build_ext --inplace && \
+pip3 install -q -r requirements.txt && \
+    black -q . && \
+    flake8 . && \
+    isort -q . && \
+    python3 setup.py -q build_ext --inplace && \
     python3 test.py && \
     cython -3 -a cmain.pyx && \
     open cmain.html
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
+
+import pretty_errors  # noqa: F401
 from Cython.Build import cythonize
 
 setup(ext_modules=cythonize("cmain.pyx"))
